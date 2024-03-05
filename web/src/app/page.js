623278e0b69e30339e0150304ec6697a858674af
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-03-05 09:29:46 -0300

    se probaron los componentes en page

--- web/src/app/page.js
+++ web/src/app/page.js
@@ -1,7 +1,16 @@
+import About from "./about.jsx/page";
+import Contact from "./contact/page";
+import Skill from "./skill/page";
+import Work from "./work/page";
+
 export default function Home() {
   return (
     <main>
-     
+      <About/>
+      <Skill/>
+      <Work/>
+      <Contact/>
+
     </main>
   );
 }
