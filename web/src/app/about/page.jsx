564535d8c436f465d08b828217e351a0d7d4bbdd
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-03-13 10:59:54 -0300

    se creo page Home con estilos, falta buttons

--- web/src/app/about/page.jsx
+++ web/src/app/about/page.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function About() {
-  return <div className="text-white">hola soy about</div>;
+  return (
+    <div className="text-white">
+      <h2>I'm Front-end developer from Argentina, Jujuy, </h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+        expedita.
+      </p>
+      <button>HIRE ME</button>
+    </div>
+  );
 }
--- web/src/app/page.js
+++ web/src/app/page.js
@@ -5,12 +5,15 @@ import Work from "./work/page";
 
 export default function Home() {
   return (
-    <main>
-      {/* <About/>
-      <Skill/>
-      <Work/>
-      <Contact/> */}
-      {/* Soy Home */}
+    <main className="flex h-full" >
+      <div className="flex flex-col text-center justify-center items-center space-y-4 text-white md:text-start">
+        <h2 className="text-3xl md:text-5xl" >I'm Front-end developer from Argentina, Jujuy, </h2>
+        <p className="text-[14px] md:text-[16px]" >
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi
+          expedita.
+        </p>
+        <button>HIRE ME</button>
+      </div>
     </main>
   );
 }
